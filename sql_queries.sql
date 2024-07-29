944c66c10ec480f02fb9ded11711177fe18262a9
kvoistinov <kvoistinov@yandex.ru>
2024-07-30 00:54:04 +0300

    Fixes

--- sql_queries.sql
+++ sql_queries.sql
@@ -118,9 +118,9 @@ ORDER BY
 SELECT 
 	InvoiceDate, BillingAddress, BillingCity, Total,
 CASE 
-	WHEN Total < 2.00 Then 'A' 
-	WHEN Total BETWEEN 2.00 AND 6.99 THEN 'B'
-	WHEN Total BETWEEN 7.00 AND 15.00 THEN 'C'
+	WHEN Total < 2.00                  THEN 'A' 
+	WHEN Total BETWEEN 2.00 AND 6.99   THEN 'B'
+	WHEN Total BETWEEN 7.00 AND 15.00  THEN 'C'
 	ELSE 'D'
 END AS PurchaseType
 FROM
